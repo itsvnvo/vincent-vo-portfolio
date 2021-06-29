5cbeba275363842254456ecbc2cc975f7da3837c
Vincent Vo <itsvnvo@gmail.com>
2021-06-29 16:06:46 -0500

    Updates

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Toggles menu
+function menuToggle(){
+    const menu = document.querySelector('.menuButton')
+    const sideMenu = document.querySelector('.sideMenu')
+    menu.classList.toggle('active')
+    sideMenu.classList.toggle('active')
+}
+
+console.log("test")
